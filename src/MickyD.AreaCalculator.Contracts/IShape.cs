ca7f3bb5fd024ab63845751d38da00ae128a0f31
Micky Duncan <micky_duncan@msn.com>
2019-10-03 08:39:37 +0800

    - first commit

---
+++ src/MickyD.AreaCalculator.Contracts/IShape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MickyD.AreaCalculator.Contracts
+{
+    public interface IShape
+    {
+        float Area {get;}
+    }
+}
